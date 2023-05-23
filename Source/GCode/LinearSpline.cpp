0d2540c449aa0cb490c509e383dd54e40c76b9d0
Nicolas Harttig <nicolas_harttig@hotmail.de>
2023-05-23 20:48:46 +0200

    unfinished

--- Source/GCode/LinearSpline.cpp
+++ Source/GCode/LinearSpline.cpp
@@ -4,9 +4,15 @@ namespace GCode
 {
   std::string LinearSpline::toString() const
   {
+    //std::string result;
+    //for (auto& point : controlPoints)
+    //{
+    //  result += "G1 X" + std::to_string(point[0]) + " Y" + std::to_string(point[1]) + " Z" + std::to_string(point[2]) + " E" + std::to_string(feedrate) + "\n";
+    //}
+
     return
 R"(
-G1 X60 Y61.3
+G1 X60 Y61.3 
 G1 Z.2
 G1 E2 F1800
 M204 S1000
--- Source/GCode/LinearSpline.h
+++ Source/GCode/LinearSpline.h
@@ -1,12 +1,17 @@
 #pragma once
 
 #include <string>
+#include <vector>
+
+#include <glm/glm.hpp>
 
 namespace GCode
 {
   class LinearSpline
   {
   public:
+    std::vector<glm::dvec3> controlPoints;    
+    double feedrate = 0.0267225;
 
     std::string toString() const;
   };
