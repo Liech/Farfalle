da9c68200dfe1a7c3a22f277e05c6256933203a3
Nicolas Harttig <nicolas_harttig@hotmail.de>
2023-09-05 21:42:24 +0200

    Macaronifyer Wraparound

--- Source/Geometry/Macaronifyer.cpp
+++ Source/Geometry/Macaronifyer.cpp
@@ -16,7 +16,12 @@ Macaronifyer::~Macaronifyer() {
 
 std::vector<std::vector<glm::dvec3>> Macaronifyer::macaronify(Model& toNoodlize) const {
   std::vector<std::vector<glm::dvec3>> result;
-  return toNoodlize.getBorder();
-    
-  //return result;
+  result = toNoodlize.getBorder();
+  
+
+  //wrap around:
+  for (int i = 0; i < result.size(); i++) {
+    result[i].push_back(result[i][0]);
+  }
+  return result;
 }
