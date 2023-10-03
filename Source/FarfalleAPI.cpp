5f502f2d8ad22118449c716db66411e10a608cdf
Liech <nicolas_harttig@hotmail.de>
2023-10-04 00:03:48 +0200

    started to fill script api

--- Source/FarfalleAPI.cpp
+++ Source/FarfalleAPI.cpp
@@ -7,14 +7,60 @@
 #include "PolyglotAPI/Source/PolyglotAPI/API/APIFunction.h"
 #include "PolyglotAPI/Source/PolyglotAPI/API/FunctionRelay.h"
 
-void FarfalleAPI::add(PolyglotAPI::API& api, PolyglotAPI::FunctionRelay& relay) {
-  std::unique_ptr<PolyglotAPI::APIFunction> create = std::make_unique<PolyglotAPI::APIFunction>("voxivoxvox", [&relay, this](const nlohmann::json& input) {
-    std::cout << "Doing great stuff" << std::endl;
+#include "Geometry/Model.h"
 
-    return input;
-    });
-  create->setDescription(R"(
-Great description
-)");
-  api.addFunction(std::move(create));
+FarfalleAPI::FarfalleAPI() {
+
+}
+
+FarfalleAPI::~FarfalleAPI() {
+
+}
+
+void FarfalleAPI::add(PolyglotAPI::API& api, PolyglotAPI::FunctionRelay& relay) {
+  //create empty voxel volume
+  std::unique_ptr<PolyglotAPI::APIFunction> createVolumeAPI = std::make_unique<PolyglotAPI::APIFunction>("createVolume", createVolume);
+  createVolumeAPI->setDescription(createVolumeDescription());
+  api.addFunction(std::move(createVolumeAPI));
+
+  //delete voxel volume
+  std::unique_ptr<PolyglotAPI::APIFunction> deleteVolumeAPI = std::make_unique<PolyglotAPI::APIFunction>("deleteVolume", deleteVolume);
+  deleteVolumeAPI->setDescription(deleteVolumeDescription());
+  api.addFunction(std::move(deleteVolumeAPI));
+
+}
+
+nlohmann::json FarfalleAPI::deleteVolume(const nlohmann::json& input) {
+  voxel.erase(input["Name"]);
+  return "";
+}
+
+std::string FarfalleAPI::createVolumeDescription() {
+  return R"(
+deletes a voxel volume
+
+createVolume({
+    'Name': 'Name',
+});
+)";
+}
+
+nlohmann::json FarfalleAPI::createVolume(const nlohmann::json& input) {
+  assert(input["Dimension"[2] % 8 == 0);
+  size_t x = input["Dimension"][0];
+  size_t y = input["Dimension"][1];
+  size_t z = input["Dimension"][2];
+  voxel[input["Name"]] = std::vector<bool>(x * y * z, false);
+  return "";
+}
+
+std::string FarfalleAPI::createVolumeDescription() {
+  return R"(
+create a named voxel volume
+
+createVolume({
+    'Name': 'Name',
+    'Dimension': [128,128,128], //dividable by 8
+});
+)";
 }
--- Source/FarfalleAPI.h
+++ Source/FarfalleAPI.h
@@ -1,11 +1,33 @@
 #pragma once
 
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <nlohmann/json.hpp>
+
 namespace PolyglotAPI {
   class API;
   class FunctionRelay;
 }
 
+class Model;
+
 class FarfalleAPI {
 public:
+  FarfalleAPI();
+  virtual ~FarfalleAPI();
+
   void add(PolyglotAPI::API&, PolyglotAPI::FunctionRelay& relay);
+
+
+private:
+  nlohmann::json createVolume(const nlohmann::json& input);
+  std::string createVolumeDescription();
+  nlohmann::json deleteVolume(const nlohmann::json& input);
+  std::string deleteVolumeDescription();
+
+  std::map<std::string, std::vector<bool>>                   voxel;
+  std::map<std::string, std::vector<std::unique_ptr<Model>>> models;
 };
