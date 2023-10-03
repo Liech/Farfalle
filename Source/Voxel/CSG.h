5a0df6f608291f99e280b34df3a8cffeaee41cbf
Liech <nicolas_harttig@hotmail.de>
2023-10-03 19:34:13 +0200

    csg added

--- Source/Voxel/CSG.h
+++ Source/Voxel/CSG.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <vector>
+#include <functional>
+
 class CSG {
 public:
-
-private:
-
+  static void andInplace(const std::vector<bool>& a, std::vector<bool>& b);
+  static void orInplace(const std::vector<bool>& a, std::vector<bool>& b);
+  static void apply(const std::vector<bool>& a, std::vector<bool>& b, std::function<bool(bool, bool)> fun);
 };
