acaf3758a95af5fcee55cd7116f037986cdebb25
Nicolas Harttig <nicolas_harttig@hotmail.de>
2024-03-21 19:08:41 +0100

    stl read/write improved

--- Source/Tools/STL.cpp
+++ Source/Tools/STL.cpp
@@ -2,7 +2,95 @@
 
 #include <fstream>
 
-void STL::write(const std::string& filename, const std::vector<glm::dvec3>& data) {
+void STL::writeUInt(std::ofstream& stream, const unsigned int& value) {
+  unsigned char bytes[4];
+  bytes[0] = ((value >> 24) & 0xFF);
+  bytes[1] = ((value >> 16) & 0xFF);
+  bytes[2] = ((value >> 8) & 0xFF);
+  bytes[3] = (value & 0xFF);
+  stream.write((const char*)(bytes), 4);
+}
+
+void STL::writeInt(std::ofstream& stream, const int& value) {
+  unsigned char bytes[4];
+  bytes[0] = ((value >> 24) & 0xFF);
+  bytes[1] = ((value >> 16) & 0xFF);
+  bytes[2] = ((value >> 8) & 0xFF);
+  bytes[3] = (value & 0xFF);
+  stream.write((const char*)(bytes), 4);
+}
+
+void STL::writeFloat(std::ofstream& stream, const float& data) {
+  const float* a = &data;
+  const int* b = (const int*)a;
+  int    c = *b;
+  writeInt(stream, data);
+}
+
+void STL::writeUShort(std::ofstream& stream, const unsigned short& value) {
+  unsigned char bytes[2];
+  bytes[0] = ((value >> 8) & 0xFF);
+  bytes[1] = (value & 0xFF);
+  stream.write((const char*)(bytes), 2);
+}
+
+unsigned int STL::readUInt(const std::vector<unsigned char>& data, size_t& position)
+{
+  unsigned char bytes[] = { data[position],data[position + 1],data[position + 2],data[position + 3] };
+  unsigned int* pInt = (unsigned int*)bytes;
+  unsigned int result = *pInt;
+  position += 4;
+  return result;
+}
+
+int STL::readInt(const std::vector<unsigned char>& data, size_t& position)
+{
+  unsigned char bytes[] = { data[position],data[position + 1],data[position + 2],data[position + 3] };
+  int* pInt = (int*)bytes;
+  int result = *pInt;
+  position += 4;
+  return result;
+}
+
+float STL::readFloat(const std::vector<unsigned char>& data, size_t& position)
+{
+  int  a = readInt(data, position);
+  int* b = &a;
+  float* c = (float*)b;
+  return *c;
+}
+
+short STL::readShort(const std::vector<unsigned char>& data, size_t& position)
+{
+  unsigned char bytes[] = { data[position],data[position + 1] };
+  short* pInt = (short*)bytes;
+  short result = *pInt;
+  position += 2;
+  return result;
+}
+
+void STL::write(const std::string& filename, const std::vector<glm::dvec3>& tris, bool binaryFormat)
+{
+  if (binaryFormat)
+    writeBinary(filename, tris);
+  else
+    writeAscii(filename, tris);
+}
+
+std::vector<glm::dvec3> STL::read(const std::string& filename)
+{
+  std::ifstream input(filename, std::ios::binary);
+  if (input.fail())
+    throw std::runtime_error("Error opening " + filename);
+  std::vector<unsigned char> buffer = std::vector<unsigned char>(std::istreambuf_iterator<char>(input), {});
+  std::string header = std::string(buffer.begin(), buffer.begin() + 5);
+  if (header == "solid")
+    return readAscii(buffer);
+  else
+    return readBinary(buffer);
+}
+
+void STL::writeAscii(const std::string& filename, const std::vector<glm::dvec3>& data) {
   std::ofstream file;
   file.open(filename);
 
@@ -39,6 +127,97 @@ void STL::write(const std::string& filename, const std::vector<glm::dvec3>& data
   file.close();
 }
 
-std::vector<glm::dvec3> STL::read(const std::string& filename) {
-  return {};
+void STL::writeBinary(const std::string& filename, const std::vector<glm::dvec3>& triangles)
+{
+  std::ofstream file;
+  file.open(filename);
+  std::string header = "FileType: Binary https://github.com/Liech                                      "; //80 byte
+  file.write(header.c_str(),header.size());
+
+  uint32_t numberOfTrianlges = triangles.size() / 3;
+  writeUInt(file, numberOfTrianlges);
+
+  for (size_t i = 0; i < numberOfTrianlges; i++)
+  {
+    glm::dvec3 a = triangles[i * 3 + 0];
+    glm::dvec3 b = triangles[i * 3 + 1];
+    glm::dvec3 c = triangles[i * 3 + 2];
+    glm::dvec3 n = glm::cross(a - b, a - c);
+
+    writeFloat (file, n[0]);
+    writeFloat (file, n[1]);
+    writeFloat (file, n[2]);
+    writeFloat (file, a[0]);
+    writeFloat (file, a[1]);
+    writeFloat (file, a[2]);
+    writeFloat (file, b[0]);
+    writeFloat (file, b[1]);
+    writeFloat (file, b[2]);
+    writeFloat (file, c[0]);
+    writeFloat (file, c[1]);
+    writeFloat (file, c[2]);
+    writeUShort(file, 0);
+  }
+  file.close();
+}
+
+std::vector<glm::dvec3> STL::readAscii(const std::vector<unsigned char>& buffer) {
+  std::vector<glm::dvec3> result;
+  std::string data = std::string(buffer.begin(), buffer.end());
+  std::istringstream iss(data);
+  std::string line;
+  std::getline(iss, line); //header is uninteresting
+  for (; std::getline(iss, line); )
+  {
+    if (line.starts_with("vertex"))
+    {
+      std::istringstream lineStream(line);
+      std::string element;
+      glm::dvec3 subresult;
+      std::getline(lineStream, element); //"vertex" -> throw away
+
+      std::getline(lineStream, element);
+      subresult.x = std::stod(element);
+      std::getline(lineStream, element);
+      subresult.y = std::stod(element);
+      std::getline(lineStream, element);
+      subresult.z = std::stod(element);
+      result.push_back(subresult);      
+    }
+  }
+
+  return result;
+}
+
+std::vector<glm::dvec3> STL::readBinary(const std::vector<unsigned char>& buffer) {
+  std::vector<glm::dvec3> result;
+  size_t position = 80; //header first 80 bytes are ignored
+  uint32_t numberOfTrianlges = readUInt(buffer, position);
+  result.reserve(numberOfTrianlges * 3);
+  
+  for (size_t i = 0; i < numberOfTrianlges; i++)
+  {
+    glm::dvec3 n;
+    n.x = readFloat(buffer, position);
+    n.y = readFloat(buffer, position);
+    n.z = readFloat(buffer, position);
+    glm::dvec3 a;
+    a.x = readFloat(buffer, position);
+    a.y = readFloat(buffer, position);
+    a.z = readFloat(buffer, position);
+    glm::dvec3 b;
+    b.x = readFloat(buffer, position);
+    b.y = readFloat(buffer, position);
+    b.z = readFloat(buffer, position);
+    glm::dvec3 c;
+    c.x = readFloat(buffer, position);
+    c.y = readFloat(buffer, position);
+    c.z = readFloat(buffer, position);
+    readShort(buffer, position); // "Attribute byte count" Not used
+    result.push_back(a);
+    result.push_back(b);
+    result.push_back(c);
+  }
+
+  return result;
 }
--- Source/Tools/STL.h
+++ Source/Tools/STL.h
@@ -3,9 +3,26 @@
 #include <glm/glm.hpp>
 #include <vector>
 #include <string>
+#include <sstream>
 
 class STL {
 public:
-  static void write(const std::string& filename, const std::vector<glm::dvec3>&);
+  static void write(const std::string& filename, const std::vector<glm::dvec3>&, bool binaryFormat = false);
   static std::vector<glm::dvec3> read(const std::string& filename);
+
+private:
+  static void writeAscii(const std::string& filename, const std::vector<glm::dvec3>&);
+  static void writeBinary(const std::string& filename, const std::vector<glm::dvec3>&);
+  static std::vector<glm::dvec3> readAscii(const std::vector<unsigned char>&);
+  static std::vector<glm::dvec3> readBinary(const std::vector<unsigned char>&);
+
+  static void writeUInt(std::ofstream& stream, const unsigned int& value);
+  static void writeInt(std::ofstream& stream, const int& value);
+  static void writeFloat(std::ofstream& stream, const float& data);
+  static void writeUShort(std::ofstream& stream, const unsigned short& value);
+
+  static unsigned int readUInt (const std::vector<unsigned char>& data, size_t& position);
+  static int          readInt  (const std::vector<unsigned char>& data, size_t& position);
+  static float        readFloat(const std::vector<unsigned char>& data, size_t& position);
+  static short        readShort(const std::vector<unsigned char>& data, size_t& position)
 };
