9344f50acbabe7f8a6aab77b63a60b143abe0d5f
Liech <nicolas_harttig@hotmail.de>
2024-03-25 23:25:14 +0100

    trace line progress

--- Source/Voxel/LineTracer.cpp
+++ Source/Voxel/LineTracer.cpp
@@ -1,15 +1,42 @@
 #include "LineTracer.h"
 
+#include <glm/gtx/hash.hpp>
 #include <unordered_set>
 
 std::vector<std::vector<glm::ivec3>> LineTracer::traceLines(const bool* data, const glm::ivec3& resolution) {
   std::vector<std::vector<glm::ivec3>> result;
 
-  auto trueBits = gatherTrues(data, resolution);
-  //std::unordered_set<glm::ivec3> trueVoxels = std::unordered_set<glm::ivec3>(trueBits.begin(), trueBits.end());
-  if (trueBits.size() != 0)
-    result.push_back(trueBits);
+  std::vector<glm::ivec3> todo = gatherTrues(data, resolution);
+  std::unordered_set<glm::ivec3> trueVoxels = std::unordered_set<glm::ivec3>(todo.begin(), todo.end());
+  std::unordered_set<glm::ivec3> visited;
+
+  std::vector<glm::ivec3> neighbourPos = getNeighbours();
+  size_t neighbourAmount               = neighbourPos.size();
+
+  while (todo.size() > 0) {
+    glm::ivec3 current = todo.back();
+    todo.pop_back();
+    visited.insert(current);
+
+    for (size_t i = 0; i < neighbourAmount; i++) {
+      glm::ivec3 nPos = neighbourPos[i] + current;
+      if (!visited.contains(nPos) && trueVoxels.contains(nPos)) {
+        //connect
+      }
+    }
+  }
+  
   return result;
+}
+
+constexpr std::vector<glm::ivec3> LineTracer::getNeighbours() {
+  std::vector<glm::ivec3> neighbours;
+  for (int x = -1; x <= 1; x++)
+    for (int y = -1; y <= 1; y++)
+      for (int z = -1; z <= 1; z++)
+        if (!(x == 0 && y == 0 && z == 0))
+          neighbours.push_back(glm::ivec3(x, y, z));
+  return neighbours;
 }
 
 std::vector<glm::ivec3> LineTracer::gatherTrues(const bool* data, const glm::ivec3& resolution) {
--- Source/Voxel/LineTracer.h
+++ Source/Voxel/LineTracer.h
@@ -7,4 +7,7 @@ class LineTracer {
 public:
   static std::vector<std::vector<glm::ivec3>> traceLines(const bool* data, const glm::ivec3& resolution); 
   static std::vector<glm::ivec3> gatherTrues(const bool* data, const glm::ivec3& resolution);
+
+private:
+  static constexpr std::vector<glm::ivec3> getNeighbours();
 };
